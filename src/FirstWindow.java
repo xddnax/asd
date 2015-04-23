579cae66574863859300015120f9ce6acd9b73fa
batousik <batousik@yandex.ru>
2015-04-23 19:46:19 +0100

    su=ync

--- src/FirstWindow.java
+++ src/FirstWindow.java
@@ -1,5 +1,42 @@
-/**
- * Created by btsyrenov on 23/04/15.
- */
-public class FirstWindow {
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.ArrayList;
+
+import javax.swing.*;
+
+public class FirstWindow extends JFrame implements ActionListener{
+    ArrayList<String> tableNames = new ArrayList<String>();
+    JPanel panel;
+
+    public FirstWindow(){
+        super("Add component on DBManipulator at runtime");
+        tableNames.add("name 1");
+        tableNames.add("bbt");
+        tableNames.add("bato");
+
+        setLayout(new BorderLayout());
+        panel = new JPanel();
+        panel.setLayout(new FlowLayout());
+        add(panel, BorderLayout.CENTER);
+        JButton button = new JButton("Get tables");
+        add(button, BorderLayout.SOUTH);
+        button.addActionListener(this);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setSize(500, 500);
+        setVisible(true);
+    }
+
+    public void actionPerformed(ActionEvent evt) {
+        for(int i=0;i<tableNames.size();i++){
+            panel.add(new JButton(tableNames.get(i)));
+            panel.revalidate();
+            validate();
+        }
+
+    }
+    public static void main(String[] args) {
+        FirstWindow fw = new FirstWindow();
+
+    }
 }
